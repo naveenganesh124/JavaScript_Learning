9bd3ac0cbe1ed4c14c3fa8c3d682960c46ef2683
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-02-24 06:58:26 +0000

    Map funtion

--- loops.js
+++ loops.js
@@ -13,3 +13,22 @@ while(i<fruits.length){
     console.log(`index : ${i}   element : ${fruits[i]}`)
         i++;        
 }
+
+
+//Inbuild loops  
+
+//Map
+
+let arr = [1,2,3,4,5,6,7,8]
+
+arr.map((item, index, array)=>{
+       console.log(`item : ${item}  index :${index}  array : ${array}`)
+})
+
+let arrayWith5 = arr.map((ele,i,arr)=>{
+         return ele* 5;
+})
+console.log(arrayWith5);
+
+
+
