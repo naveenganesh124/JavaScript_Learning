7c4a7d33d67537ad0bf927f76b3fea48048cf489
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-02-24 06:46:24 +0000

    for and while loop

---
+++ loops.js
@@ -0,0 +1,15 @@
+let fruits = ["apple","bannana", "orange","cherry"];
+
+
+//for loop
+for(let i =0 ; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+//while loop
+
+let i = 0;
+while(i<fruits.length){
+    console.log(`index : ${i}   element : ${fruits[i]}`)
+        i++;        
+}
