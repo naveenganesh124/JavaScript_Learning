9feaddd37d2f0d796aeef672acfc9bd26dc60004
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-02-24 05:59:01 +0000

    Creating array in two methods.

---
+++ ArrayCreation.js
@@ -0,0 +1,8 @@
+let arr = new Array();
+arr.push(5, 5);
+console.log(` arr  : ${arr}`);
+
+//----Easy array creation 
+
+let easyArrCreation = [5, 6]
+console.log(`easyArrCreation ${easyArrCreation}`);
