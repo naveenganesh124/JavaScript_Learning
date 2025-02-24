592425c912f30f3c186fabdb970e89becea7cf81
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-02-24 06:13:59 +0000

    object into  array and lenth property

--- ArrayCreation.js
+++ ArrayCreation.js
@@ -6,3 +6,21 @@ console.log(` arr  : ${arr}`);
 
 let easyArrCreation = [5, 6]
 console.log(`easyArrCreation ${easyArrCreation}`);
+
+
+//- accesing the array element
+
+let fruits =["apple","orange","banana"];  //index value starts from 0
+console.log(fruits[1]);   
+
+//Array will allow object also
+ 
+fruitWithExpiryDate ={
+    fruitName : "pineapple",
+    expiryDate : "13.12.2025"
+}
+
+fruits1 = ["apple","bannana", "orange" , fruitWithExpiryDate.fruitName];
+
+console.log(fruits1);
+console.log(fruits1.length); //for getting the length of an array
