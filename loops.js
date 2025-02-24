bc5d3f683ba90d93a53176ed4cad88a9d891f3d1
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-02-24 07:56:39 +0000

    Map and reduce

--- loops.js
+++ loops.js
@@ -19,16 +19,53 @@ while(i<fruits.length){
 
 //Map
 
-let arr = [1,2,3,4,5,6,7,8]
+let arrMap = [1,2,3,4,5,6,7,8]
 
-arr.map((item, index, array)=>{
+arrMap.map((item, index, array)=>{
        console.log(`item : ${item}  index :${index}  array : ${array}`)
 })
 
-let arrayWith5 = arr.map((ele,i,arr)=>{
-         return ele* 5;
+let arrayWith5 = arrMap.map((ele,i,arr)=>{            // it will return array element * 5
+         return ele* 5;                        
 })
-console.log(arrayWith5);
+console.log(arrayWith5);       
+
+
+//filter
+
+let arrFilter = [1,2,3,4,5,6,7,8,9,10]
+
+arrFilter.filter((item, index, array)=>{
+    console.log(`item : ${item}  index :${index}  array : ${array}`)
+})
+ 
+let arrayWitheven = arrFilter.filter((ele)=>{                // it will return the value greater than 3
+     return ele > 3;
+})
+
+console.log(arrayWitheven);
+
+
+//reduce
+
+// array.reduce((previousValue, currentValue, currentIndex, array) => {
+//     // Your logic
+//  }, initialValue);
+ 
+
+let arrReduce = [1,2,3,4,5,6,7,8,9,10]
+
+arrReduce.reduce((previousValue,currrentValue ,currenIndex, array )=>{
+      console.log(`previousValue : ${previousValue},  currrentValue : ${currrentValue} ,   currenIndex : ${currenIndex}, array : ${array}`)
+      return currrentValue;
+},0)
+
+let sumArray = arrReduce.reduce((prev, current)=>{  // it will return the sum of the array.
+    return prev + current;
+},0)
+
+console.log(sumArray);
+
 
 
 
